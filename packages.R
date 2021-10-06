58012c6d28a0cbf38ba84eda956573aac5168b3c
inmacc96 <inmacaballerocarrero@gmail.com>
2021-10-06 19:28:21 +0200

    Create script with the packages we need to load for this course.

---
+++ packages.R
@@ -0,0 +1,11 @@
+# Packages required for the course
+
+install.packages("tidyverse")
+#Para saber si están todos los paquetes actualizados ejecutar: tidyverse_update()
+
+#Paquetes que nos proporcionan información sobre:
+# 1. gapminder: Desarrollo mundial
+# 2. nycflights13: Datos de vuelos de una aereolinea
+# 3. lahman: Datos del beisbol
+
+install.packages(c("gapminder","nycflights13","Lahman"))
